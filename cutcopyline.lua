29182734d460bf8330cb78edfd51513396d6f391
luveti <tu4756@gmail.com>
2020-04-20 22:00:40 -0400

    Fixed issue in cutcopyline plugin
    - also swapped if statement around

--- cutcopyline.lua
+++ cutcopyline.lua
@@ -14,17 +14,19 @@ command.map["doc:copy"] = nil
 command.add(nil, {
   ["doc:cut"] = function()
     local line1, col1, line2, col2 = doc():get_selection(true)
-    if line1 ~= line2 or col1 ~= col2 then
-      return cut_command()
+    if line1 == line2 and col1 == col2 then
+      system.set_clipboard(doc():get_text(line1, 1, line1 + 1, 1))
+      doc():remove(line1, 1, line1 + 1, 1)
+     else
+      cut_command.perform()
     end
-    system.set_clipboard(doc():get_text(line1, 1, line1 + 1, 1))
-    doc():remove(line1, 1, line1 + 1, 1)
   end,
   ["doc:copy"] = function()
     local line1, col1, line2, col2 = doc():get_selection(true)
-    if line1 ~= line2 or col1 ~= col2 then
-      return copy_command()
+    if line1 == line2 and col1 == col2 then
+      system.set_clipboard(doc():get_text(line1, 1, line1 + 1, 1))
+    else
+      copy_command.perform()
     end
-    system.set_clipboard(doc():get_text(line1, 1, line1 + 1, 1))
   end,
 })
